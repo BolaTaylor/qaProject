e2e695305e4480c4fb29e0ba29882413d38aaa24
bolataylor <bolataylor7@gmail.com>
2018-06-04 01:08:33 +0100

    Refactoring Changes

---
+++ src/main/java/com/qa/dao/UserAccountsDao.java
@@ -0,0 +1,69 @@
+package com.qa.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserAccountsDao {
+
+    static UserAccounts userAccounts = new UserAccounts();
+    static UserAccounts userAccountsOne = new UserAccounts();
+    static UserAccounts userAccountsTwo = new UserAccounts();
+    static UserAccounts userAccountsThree = new UserAccounts();
+
+    static List<UserAccounts> userAccountsList = new ArrayList<>();
+
+    static {
+        initUserAccounts();
+    }
+
+    private static void initUserAccounts(){
+
+        if (!checkUserAccount(userAccounts)) {
+            userAccounts.setId(1);
+            userAccounts.setFirstName("John");
+            userAccounts.setLastName("Taylor");
+            userAccounts.setAccountNumber("12345");
+            userAccountsList.add(userAccounts);
+        }
+        if (!checkUserAccount(userAccountsOne)) {
+            userAccountsOne.setId(2);
+            userAccountsOne.setFirstName("Mary");
+            userAccountsOne.setLastName("Williams");
+            userAccountsTwo.setLastName("Peterson");
+            userAccountsOne.setAccountNumber("23456");
+            userAccountsList.add(userAccountsOne);
+
+        }
+
+        if (!checkUserAccount(userAccountsTwo)) {
+            userAccountsTwo.setId(3);
+            userAccountsTwo.setFirstName("Fred");
+            userAccountsTwo.setLastName("Peterson");
+            userAccountsTwo.setAccountNumber("45678");
+            userAccountsList.add(userAccountsTwo);
+
+        }
+        if (!checkUserAccount(userAccountsThree)) {
+            userAccountsThree.setId(4);
+            userAccountsThree.setFirstName("Peter");
+            userAccountsThree.setLastName("Bauer");
+            userAccountsThree.setAccountNumber("98765");
+            userAccountsList.add(userAccountsThree);
+
+        }
+    }
+    public static boolean checkUserAccount(UserAccounts userAccounts){
+
+        for(UserAccounts userAccountObject: userAccountsList){
+            if(userAccountObject.getId() == userAccounts.getId()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static List<UserAccounts> getAllUserAccounts() {
+        return userAccountsList;
+    }
+
+}
--- src/main/java/com/qa/service/UserAccountService.java
+++ src/main/java/com/qa/service/UserAccountService.java
@@ -1,64 +1,22 @@
 package com.qa.service;
 
 import com.qa.dao.UserAccounts;
+import com.qa.dao.UserAccountsDao;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserAccountService {
 
-
     private List<UserAccounts> userAccountsList = new ArrayList<>();
-    private UserAccounts userAccounts  = new UserAccounts();
-    private UserAccounts userAccountsOne = new UserAccounts();
-    private UserAccounts userAccountsTwo = new UserAccounts();
-    private UserAccounts userAccountsThree = new UserAccounts();
-
-    public UserAccountService(){
-       if(!checkUserAccount(userAccounts)) {
-           userAccounts.setId(1);
-           userAccounts.setFirstName("John");
-           userAccounts.setLastName("Taylor");
-           userAccounts.setAccountNumber("12345");
-           userAccountsList.add(userAccounts);
-       }
-       if(!checkUserAccount(userAccountsOne)) {
-           userAccountsOne.setId(2);
-           userAccountsOne.setFirstName("Mary");
-           userAccountsOne.setLastName("Williams");
-           userAccountsTwo.setLastName("Peterson");
-           userAccountsOne.setAccountNumber("23456");
-           userAccountsList.add(userAccountsOne);
-
-       }
-
-       if(!checkUserAccount(userAccountsTwo)) {
-           userAccountsTwo.setId(3);
-           userAccountsTwo.setFirstName("Fred");
-           userAccountsTwo.setLastName("Peterson");
-           userAccountsTwo.setAccountNumber("45678");
-           userAccountsList.add(userAccountsTwo);
-
-       }
-       if(!checkUserAccount(userAccountsThree)) {
-           userAccountsThree.setId(4);
-           userAccountsThree.setFirstName("Peter");
-           userAccountsThree.setLastName("Bauer");
-           userAccountsThree.setAccountNumber("98765");
-           userAccountsList.add(userAccountsThree);
-
-       }
-
-
-   }
 
     public List<UserAccounts> getUserAccounts(){
-        return this.userAccountsList;
+        return UserAccountsDao.getAllUserAccounts();
     }
 
 
     public void addAccounts(UserAccounts userAccounts){
-    if(!checkUserAccount(userAccounts)){
+    if(!UserAccountsDao.checkUserAccount(userAccounts)){
         userAccounts.setId(userAccountsList.size() + 1);
         userAccountsList.add(userAccounts);
     }
@@ -66,20 +24,11 @@ public class UserAccountService {
     }
 
     public void deleteAccounts(UserAccounts userAccounts){
-        if(!checkUserAccount(userAccounts)){
+        if(!UserAccountsDao.checkUserAccount(userAccounts)){
             userAccountsList.remove(userAccounts);
         }
 
     }
 
-    private boolean checkUserAccount(UserAccounts userAccounts){
-
-    for(UserAccounts userAccountObject: userAccountsList){
-        if(userAccountObject.getId() == userAccounts.getId()){
-            return true;
-        }
-    }
-    return false;
-    }
 
 }
