13e54f9ffcfeff3df2f9331259a70816086bc502
bolataylor <bolataylor7@gmail.com>
2018-06-04 03:50:11 +0100

    removed unecessary imports

--- src/main/java/com/qa/main/UserAccountApplication.java
+++ src/main/java/com/qa/main/UserAccountApplication.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
